a89f3e12e1a8f47681b90cacd7cac4cceefbf5e0
mcattx <mcattx@outlook.com>
2018-01-27 11:39:11 +0800

    re: refactor drop.js code

--- src/drop.js
+++ src/drop.js
@@ -1,46 +1,20 @@
 const path = nodeRequire('path')
 const spriteTask = nodeRequire(path.join(__dirname, '/tasks/sprite.js'));
 
-var dropArea = document.getElementById('drop');
-
-function dragHandler(e) {
-
-    var info = '',
-        outputEl = document.getElementById('output'),
-        files,
-        i,
-        len;
+document.addEventListener('drop', (e) => {
     e.preventDefault();
-    if (e.type === 'dragenter') {
-        dropArea.classList.add('enter')
+    e.stopPropagation();
+
+    for (let f of e.dataTransfer.files) {
+        const sourePath = f.path;
+        const destPath = path.join(__dirname, '/output', f.name);
+        spriteTask(sourePath, destPath, function() {
+            console.log('SpriteTask Done')
+        })
     }
+})
 
-    if (e.type == 'drop') {
-        files = e.dataTransfer.files;
-        i = 0;
-        len = files.length;
-
-        if (len == 1) {
-            let temp = files[0];
-            if (temp.type === '') {
-                // 说明是目录
-                spriteTask(temp.path, path.join(__dirname, '/testSprite'), function() {
-                    console.log('SpriteTask Done')
-                })
-            }
-        }
-
-        // while (i < len) {
-        //     console.log(files[i])
-        //     info += files[i].name + "( " + files[i].type + ", " + files[i].size + " bytes) <br>";
-        //     i++;
-        // }
-        // outputEl.innerHTML = info
-    }
-
-    
-}
-
-dropArea.addEventListener('dragenter', dragHandler, false);
-dropArea.addEventListener('dragover', dragHandler, false);
-dropArea.addEventListener('drop', dragHandler, false);
+document.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+})
