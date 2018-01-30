14ed69695249fa9b7eaa883297a623757d88824f
mcattx <mcattx@outlook.com>
2018-01-30 22:04:33 +0800

    fea: determine whether the directory is empty

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,30 @@
 const path = nodeRequire('path')
-const spriteTask = nodeRequire(path.join(__dirname, '/_tasks/sprite.js'));
+const spriteTask = nodeRequire(path.join(__dirname, '/_tasks/sprite.js'))
+const fsHelper = nodeRequire(path.join(__dirname, '../js/_helpers/fs_helper.js'))
 
 const $ = function(el) {
     return document.querySelectorAll(el)[0]
+}
+
+/**
+ * 
+ * @param {String} info - Toast content
+ * @param {String} type - Toast type. including 'error' and 'info' as default.
+ */
+function showToast(info, type) {
+    $('.toast').innerHTML = info
+    if (type === 'error') {
+        $('.toast').classList.add('error')
+    }
+    $('.toast').classList.add('show')
+
+    setTimeout(() => {
+        if (type === 'error') {
+            $('.toast').classList.remove('error')
+        }
+        $('.toast').classList.remove('show')
+        $('.toast').innerHTML = ''
+    }, 2000);
 }
 
 document.addEventListener('drop', (e) => {
@@ -12,19 +34,26 @@ document.addEventListener('drop', (e) => {
     for (let f of e.dataTransfer.files) {
         const sourePath = f.path;
         const destPath = path.join(__dirname, '../../../output', f.name);
+    
+        fsHelper.checkEmptyDir(sourePath).then(() => {
+            if (f.name) {
+                $('.dir-info').innerHTML = 'Directory ' + f.name + ' loaded.'
+                $('.dir-info').classList.add('show')
+            }
+            spriteTask(sourePath, destPath, function() {
+                showToast('Build Sprite Successfully.')
+            })
+        }).catch((err) => {
+            showToast('The folder is empty!', 'error')
+        })
+        
+        return false;
         if (f.name) {
             $('.dir-info').innerHTML = 'Directory ' + f.name + ' loaded.'
             $('.dir-info').classList.add('show')
         }
         spriteTask(sourePath, destPath, function() {
-
-            $('.toast').innerHTML = 'Build Sprite Successfully.'
-            $('.toast').classList.add('show')
-
-            setTimeout(() => {
-                $('.toast').classList.remove('show')
-                $('.toast').innerHTML = ''
-            }, 2000);
+            showToast('Build Sprite Successfully.')
         })
     }
 })
