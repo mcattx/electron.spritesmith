3d231d6e172d9dbe4bbe43f77e86b3fbac991320
mcattx <mcattx@outlook.com>
2018-01-30 23:41:39 +0800

    re: refator part of code in fs_helper.js

--- src/assets/js/_helpers/fs_helper.js
+++ src/assets/js/_helpers/fs_helper.js
@@ -49,17 +49,17 @@ function checkEmptyDir(filePath) {
  * @returns {Object}
  */
 function getFileMimeType (filePath){
-    var buffer = new Buffer(8);
-    var fd = fs.openSync(filePath, 'r');
+    let buffer = new Buffer(8);
+    let fd = fs.openSync(filePath, 'r');
     fs.readSync(fd, buffer, 0, 8, 0);
-    var newBuf = buffer.slice(0, 4);
-    var head_1 = newBuf[0].toString(16);
-    var head_2 = newBuf[1].toString(16);
-    var head_3 = newBuf[2].toString(16);
-    var head_4 = newBuf[3].toString(16);
-    var typeCode = head_1 + head_2 + head_3 + head_4;
-    var filetype = '';
-    var mimetype;
+    let newBuf = buffer.slice(0, 4);
+    let head_1 = newBuf[0].toString(16);
+    let head_2 = newBuf[1].toString(16);
+    let head_3 = newBuf[2].toString(16);
+    let head_4 = newBuf[3].toString(16);
+    let typeCode = head_1 + head_2 + head_3 + head_4;
+    let filetype = '';
+    let mimetype;
     switch (typeCode){
         case 'ffd8ffe1':
             filetype = 'jpg';
